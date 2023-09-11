241449dccbf73e1386326720e8b2efc91ab05e4f
omotrium <omo4adex@gmail.com>
2023-09-11 02:02:45 +0100

    added dockerfile

--- src/main/java/com/javawhizz/App/customer/CustomerController.java
+++ src/main/java/com/javawhizz/App/customer/CustomerController.java
@@ -2,22 +2,66 @@ package com.javawhizz.App.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/customers")
+//@RequestMapping("api/v1/customers")
+@RequestMapping("/info")
 public class CustomerController {
-    private static final List<Customer> CUSTOMERS = List.of(
-            new Customer(1L, "john", "doe", "john@javawhizz.com"),
-            new Customer(2L, "mary", "public", "mary@javawhizz.com"),
-            new Customer(3L, "elon", "musk","elon@javawhizz.com"),
-            new Customer(4L, "dunny","duncan","dunny@javawhizz.com")
-    );
+//    private static final List<Customer> CUSTOMERS = List.of(
+//            new Customer(1L, "john", "doe", "john@javawhizz.com"),
+//            new Customer(2L, "mary", "public", "mary@javawhizz.com"),
+//            new Customer(3L, "elon", "musk","elon@javawhizz.com"),
+//            new Customer(4L, "dunny","duncan","dunny@javawhizz.com")
+//    );
 
-    @GetMapping
-    public List<Customer> findAllCustomers(){
-        return CUSTOMERS;
-    }
+//    @GetMapping
+//    //public List<Customer> findAllCustomers(){
+//        return CUSTOMERS;
+//    }
+
+        @GetMapping
+        public Endpoint EndpointInformation(
+                @RequestParam String slack_name,
+                @RequestParam String track)
+        {
+            LocalDateTime currentTime = LocalDateTime.now(ZoneOffset.UTC);
+            DayOfWeek currentDay = currentTime.getDayOfWeek();
+            String formattedTime = currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+//        DateTime formattedTime = DateTime.now( DateTimeZone.UTC );
+
+            String githubFileUrl = "https://github.com";
+            String githubRepoUrl = "https://github.com";
+
+            return new Endpoint(
+                    slack_name,
+                    currentDay.toString(),
+                    formattedTime,
+                    track,
+                    githubFileUrl,
+                    githubRepoUrl,
+                    200
+            );
+
+
+//        Endpoint endpoint = new Endpoint();
+//        endpoint.setSlackName("slack_Name");
+//        endpoint.setCurrentDay(LocalDateTime.now().getDayOfWeek().toString());
+//        endpoint.setUtcTime(LocalDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME));
+//        endpoint.setTrack(track);
+//        endpoint.setGithubFileUrl("https://github.com");
+//        endpoint.setGithubRepoUrl("https://github.com");
+//
+//        endpoint.setStatusCode(200);
+
+
+        }
+
 }
